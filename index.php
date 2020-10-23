02dba9985565b65ffbd92e367f44d11c86531b24
Parker Freestone <pnfreestone2@gmail.com>
2020-10-23 16:21:15 -0600

    First commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FAILURE HOME</title>
+
+    <script src="https://kit.fontawesome.com/b0bbf5b377.js" crossorigin="anonymous"></script>
+
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Sedgwick+Ave+Display&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="styles/landing.css">
+
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="left">
+            <div class="logo">
+                <img src="./logos/logo-red.png" alt="FAILURE SKATEBOARDING">
+            </div>
+        </div>
+
+        <div class="right">
+            <div class="nav">
+                <div class="link">
+                    <a href="index.html"> HOME </a>
+                </div>
+                <div class="link">
+                    <a href="index.html"> ABOUT US </a>
+                </div>
+                <div class="link">
+                    <a href="index.html"> SHOP </a>
+                </div>
+                <div class="link">
+                    <a href="index.html"> CONTACT </a>
+                </div>
+            </div>
+            <div class="info-1">
+                <div class="header">
+                    WELCOME
+                </div>
+                <div class="description">
+                    <div class="50">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni nihil quibusdam recusandae libero expedita voluptate officiis voluptates? Aut porro blanditiis praesentium. Mollitia laudantium itaque dolores enim iste consequatur perspiciatis necessitatibus.</div>
+                </div>
+            </div>
+            <div class="info-2">
+                <div class="email">
+                    failureskatingco@gmail.com
+                </div>
+                <div class="links">
+                    <div class="link">
+                        <a href="https://www.instagram.com"> <i class="fab fa-instagram-square"></i> </a>
+                    </div>
+                    <div class="link">
+                        <a href="https://www.twitter.com"> <i class="fab fa-twitter-square"></i> </a>
+                    </div>
+                    <div class="link">
+                        <a href="https://www.facebook.com"> <i class="fab fa-facebook-square"></i> </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
